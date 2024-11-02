a7442c987fcdb9903e350b60159a845c3f414351
Jared Keh <keh19001@byui.edu>
2024-11-02 17:15:36 -0600

    auth0

--- server.js
+++ server.js
@@ -5,6 +5,31 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 const app = express();
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'N1hsZzc6qVDsjcxfqnr1qlmNxWDX50R2',
+  issuerBaseURL: 'https://dev-yob0ogn6bg5av384.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json()); // Ensure JSON body parsing middleware is used
