bcf948d1e2455a813d6ea81e1813e70d55be94ba
Jared Keh <keh19001@byui.edu>
2024-11-09 14:16:20 -0700

    swagger and server change

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const { auth, requiresAuth } = require('express-openid-connect');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+  };
+/**
+ * @swagger
+ * components:
+ *    securitySchemes:
+ *        OpenID:
+ *          type: openIdConnect
+ *          openIdConnectUrl: https://dev-yob0ogn6bg5av384.us.auth0.com
+ */
+router.use(auth(config))
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Get an authentications state
+ *     description: Retrieve authentication
+ *     responses:
+ *       200:
+ *         description: Successful response authentication
+ */
+// req.isAuthenticated is provided from the auth router
+router.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in BOI' : 'Logged out');
+  });
+
+  /**
+ * @swagger
+ * /profile:
+ *   get:
+ *     summary: Get an user profile
+ *     description: Retrieve profile
+ *     security:
+ *     - OpenId: []
+ *     responses:
+ *       200:
+ *         description: Successful response with profile
+ */
+
+router.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user, null, 2));
+  });
 
 router.use('/', require('./swagger'));
 router.use('/restaurants', require('./restaurants'))
--- server.js
+++ server.js
@@ -3,34 +3,11 @@ const mongodb = require('./db/connect');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const { auth } = require('express-openid-connect');
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.SECRET,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL
-};
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
-
-const { requiresAuth } = require('express-openid-connect');
-
-app.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
 
 const port = process.env.PORT || 3000;
 
@@ -42,7 +19,10 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use('/', require('./routes'));
+app
+  .use('/', require('./routes'))
+  .use(express.urlencoded({ extended:true }))
+  .use(cors());
 
 mongodb.initDb((err) => {
   if (err) {
